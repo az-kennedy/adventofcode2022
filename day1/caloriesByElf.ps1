c6e3710eccf6b7616b1251d75eb6c71391ee41bc
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2022-12-06 16:38:10 -0500

    added send star for day 1

--- day1/caloriesByElf.ps1
+++ day1/caloriesByElf.ps1
@@ -19,6 +19,7 @@
 
 $data = Get-Content -Path '/Users/tkennedy/REPOS/adventofcode2022/day1/calories.txt'
 
+$count = 1
 $fattestElfHash = @{}
 $caloriesCnt = 0
 
@@ -26,14 +27,18 @@ foreach ($row in $data) {
   if ($row -ne "") {
     $caloriesCnt += $row
   } else {
-
-    
-    if ($caloriesCnt -gt $fattestElf) {
-      $fattestElf = $caloriesCnt
-      $caloriesCnt = 0
-    } else {
-      $caloriesCnt = 0
-    }
+    $fattestElfHash["Elf $count"] = $caloriesCnt
+    $caloriesCnt = 0
+    $count += 1
   }
 }
-return "Total Calories carried by the Fattest Elf: $fattestElf"
+
+# Top 3 elves with most calories:
+$sorted = $fattestElfHash.GetEnumerator() | Sort-Object -Property Value
+
+$sum = 0
+foreach ($row in $($sorted[-1..-3])){
+  $sum += $row.Value
+}
+
+return $sum
