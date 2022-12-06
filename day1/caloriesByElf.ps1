517176718a65a0dcc187fc5f03d55f3aaa063b6d
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2022-12-06 16:23:45 -0500

    updated

--- day1/caloriesByElf.ps1
+++ day1/caloriesByElf.ps1
@@ -1,12 +1,33 @@
+# $data = Get-Content -Path '/Users/tkennedy/REPOS/adventofcode2022/day1/calories.txt'
+
+# $fattestElf = 0
+# $caloriesCnt = 0
+
+# foreach ($row in $data) {
+#   if ($row -ne "") {
+#     $caloriesCnt += $row
+#   } else {
+#     if ($caloriesCnt -gt $fattestElf) {
+#       $fattestElf = $caloriesCnt
+#       $caloriesCnt = 0
+#     } else {
+#       $caloriesCnt = 0
+#     }
+#   }
+# }
+# return "Total Calories carried by the Fattest Elf: $fattestElf"
+
 $data = Get-Content -Path '/Users/tkennedy/REPOS/adventofcode2022/day1/calories.txt'
 
-$fattestElf = 0
+$fattestElfHash = @{}
 $caloriesCnt = 0
 
 foreach ($row in $data) {
   if ($row -ne "") {
     $caloriesCnt += $row
   } else {
+
+    
     if ($caloriesCnt -gt $fattestElf) {
       $fattestElf = $caloriesCnt
       $caloriesCnt = 0
