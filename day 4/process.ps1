1222652256b387e2c526d18919169c7fdcd818bf
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2022-12-08 14:32:54 -0500

    completed day 4

--- day 4/process.ps1
+++ day 4/process.ps1
@@ -1,4 +1,28 @@
-$data = Get-Content -Path '/Users/tkennedy/REPOS/adventofcode2022/day 3/input.txt'
+$data = Get-Content -Path '/Users/tkennedy/REPOS/adventofcode2022/day 4/input.txt'
 
-$sum = 0
+$overlapping = 0
 
+foreach ($row in $data) {
+  $split = $row.Split(',')
+
+  $elf1 = $split[0].split('-')
+  $elf2 = $split[1].split('-')
+
+  # compare the elf cleaning zones between eachother
+  Write-Host "
+  Elf1 : $elf1
+  Elf2 : $elf2
+  "
+  if (([int]$elf1[0] -le [int]$elf2[0]) -and ([int]$elf1[1] -ge [int]$elf2[1])) {
+    $overlapping += 1
+  } else {
+    if (([int]$elf1[0] -ge [int]$elf2[0]) -and ([int]$elf1[1] -le [int]$elf2[1])) {
+      $overlapping += 1
+    }
+  }
+
+  
+
+}
+
+return $overlapping
